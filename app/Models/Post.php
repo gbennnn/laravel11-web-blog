07bb5c095149e31814e7b4e592e7a8335c454a95
Benony Gabriel <gbenn.work@gmail.com>
2024-11-26 20:45:21 +0700

    create Post model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,9 +2,20 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    use HasFactory;
+    protected $table = 'posts';
+    protected $fillable = [
+        'title',
+        'slug',
+        'description',
+        'content',
+        'status',
+        'thumbnail',
+        'user_id',
+    ];
 }
