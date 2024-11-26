afbdc913e506690cc6891ac6c4436304fb443081
Benony Gabriel <gbenn.work@gmail.com>
2024-11-27 01:17:34 +0700

    feat(blogs): update edit link to point to the correct blog edit route in member area

--- resources/views/member/blogs/index.blade.php
+++ resources/views/member/blogs/index.blade.php
@@ -34,7 +34,8 @@
                                     <td class="border px-6 py-4 text-center text-sm hidden lg:table-cell">
                                         {{ $value->status }}</td>
                                     <td class="border px-6 py-4 text-center">
-                                        <a href='' class="text-blue-600 hover:text-blue-400 px-2">edit</a>
+                                        <a href='{{ url('member/blogs/' . $value->id . '/edit') }}'
+                                            class="text-blue-600 hover:text-blue-400 px-2">edit</a>
                                         <a href='' class="text-blue-600 hover:text-blue-400 px-2">lihat</a>
                                         <button type=' submit' class='text-red-600 hover:text-red-400 px-2'>
                                             hapus
