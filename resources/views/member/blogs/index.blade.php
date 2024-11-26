5e3862a490b1e78d28dbf4077d7794af2a77e24f
Benony Gabriel <gbenn.work@gmail.com>
2024-11-27 01:35:37 +0700

    fix(blogs): update edit link to use named route for blog posts in member area

--- resources/views/member/blogs/index.blade.php
+++ resources/views/member/blogs/index.blade.php
@@ -34,7 +34,7 @@
                                     <td class="border px-6 py-4 text-center text-sm hidden lg:table-cell">
                                         {{ $value->status }}</td>
                                     <td class="border px-6 py-4 text-center">
-                                        <a href='{{ url('member/blogs/' . $value->id . '/edit') }}'
+                                        <a href='{{ route('member.blogs.edit', ['blog' => $value->id]) }}'
                                             class="text-blue-600 hover:text-blue-400 px-2">edit</a>
                                         <a href='' class="text-blue-600 hover:text-blue-400 px-2">lihat</a>
                                         <button type=' submit' class='text-red-600 hover:text-red-400 px-2'>
