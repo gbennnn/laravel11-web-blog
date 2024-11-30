1b94af0980a6a67e5f924444aa3e4bc16e6f49ca
Benony Gabriel <gbenn.work@gmail.com>
2024-11-30 12:40:46 +0700

    fix(blogs): update comment from 'title' to 'Judul' in edit blog view

--- resources/views/member/blogs/edit.blade.php
+++ resources/views/member/blogs/edit.blade.php
@@ -24,7 +24,7 @@
                             @csrf
                             @method('PUT')
 
-                            {{-- Kolom edit title --}}
+                            {{-- Kolom edit Judul --}}
                             <div>
                                 <x-input-label for="title" value="Title" />
                                 <x-text-input id="title" name="title" type="text" class="mt-1 block w-full"
