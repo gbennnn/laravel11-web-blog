67926bf977d9994f81a612fe5b322ac1499b6eac
Benony Gabriel <gbenn.work@gmail.com>
2024-11-30 21:47:06 +0700

    feat(blogs): add 'Tambah Tulisan' button to blog settings header

--- resources/views/member/blogs/index.blade.php
+++ resources/views/member/blogs/index.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            Pengaturan Blog
+            Pengaturan Blog <a href="{{ route('member.blogs.create') }}" class="bg-blue-500 p-2 rounded-md text-white text-sm">Tambah Tulisan</a>
         </h2>
     </x-slot>
 
