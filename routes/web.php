302d154e8a2a992e042b08a9cb20688edb090140
Benony Gabriel <gbenn.work@gmail.com>
2024-12-01 18:50:37 +0700

    feat(home): update home route to use new home page component

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\Member\BlogController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('components.front.home-page');
 });
 
 Route::get('/dashboard', function () {
