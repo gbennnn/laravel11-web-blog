29f4e589652f5cf0e5c1bc8ad04b09ad99084bdc
Benony Gabriel <gbenn.work@gmail.com>
2024-12-05 14:01:22 +0700

    feat(layout): add navigation component and main layout for the front end

--- app/Http/Controllers/Member/BlogController.php
+++ app/Http/Controllers/Member/BlogController.php
@@ -2,12 +2,13 @@
 
 namespace App\Http\Controllers\Member;
 
+use Log;
 use App\Models\Post;
-use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Gate;
 
 class BlogController extends Controller
 {
@@ -22,8 +23,8 @@ class BlogController extends Controller
         // exit();
 
         // dd($user);
-        $data = Post::where('user_id', $user->id)->where(function($query) use ($search){
-            if($search){
+        $data = Post::where('user_id', $user->id)->where(function ($query) use ($search) {
+            if ($search) {
                 $query->where('title', 'like', "%{$search}%")->orWhere('content', 'like', "%{$search}%");
             }
         })->orderBy('id', 'desc')->paginate(10)->withQueryString();
@@ -47,35 +48,35 @@ class BlogController extends Controller
     {
         // Untuk memvalidasi data yang dikirim
         $request->validate([
-            'title'=>'required',
-            'content'=>'required',
-            'thumbnail'=>'required|image|mimes:jpeg,png,jpg|max:10240',
+            'title' => 'required',
+            'content' => 'required',
+            'thumbnail' => 'required|image|mimes:jpeg,png,jpg|max:10240',
         ], [
-            'title.required'=>'Judul wajib diisi',
-            'content.required'=>'Konten wajib diisi',
-            'thumbnail.required'=>'Thumbnail wajib diisi',
-            'thumbnail.image'=>'Hanya file gambar yang diperbolehkan',
-            'thumbnail.mimes'=>'Ekstensi file yang diperbolehkan: jpeg, png, jpg',
-            'thumbnail.max'=>'Ukuran file maksimal 10MB',
+            'title.required' => 'Judul wajib diisi',
+            'content.required' => 'Konten wajib diisi',
+            'thumbnail.required' => 'Thumbnail wajib diisi',
+            'thumbnail.image' => 'Hanya file gambar yang diperbolehkan',
+            'thumbnail.mimes' => 'Ekstensi file yang diperbolehkan: jpeg, png, jpg',
+            'thumbnail.max' => 'Ukuran file maksimal 10MB',
         ]);
 
         // Cek apakah ada file thumbnail yang dikirim
-        if($request->hasFile('thumbnail')){
+        if ($request->hasFile('thumbnail')) {
             $image = $request->file('thumbnail');
-            $imageName = time().'_'.$image->getClientOriginalName();
+            $imageName = time() . '_' . $image->getClientOriginalName();
             $destinationPath = public_path(getenv('CUSTOM_THUMBNAIL_LOCATION'));
             $image->move($destinationPath, $imageName);
         }
 
         // Insert data
-        $data=[
-            'title'=>$request->title,
-            'description'=>$request->description,
-            'content'=>$request->content,
-            'status'=>$request->status,
-            'thumbnail'=>$imageName,
-            'slug'=>$this->generateSlug($request->title),
-            'user_id'=>Auth::id()
+        $data = [
+            'title' => $request->title,
+            'description' => $request->description,
+            'content' => $request->content,
+            'status' => $request->status,
+            'thumbnail' => $imageName,
+            'slug' => $this->generateSlug($request->title),
+            'user_id' => Auth::id()
         ];
 
         // Insert data ke database
@@ -112,39 +113,39 @@ class BlogController extends Controller
     {
         // Untuk memvalidasi data yang dikirim
         $request->validate([
-            'title'=>'required',
-            'content'=>'required',
-            'thumbnail'=>'image|mimes:jpeg,png,jpg|max:10240',
+            'title' => 'required',
+            'content' => 'required',
+            'thumbnail' => 'image|mimes:jpeg,png,jpg|max:10240',
         ], [
-            'title.required'=>'Judul wajib diisi',
-            'content.required'=>'Konten wajib diisi',
-            'thumbnail.image'=>'Hanya file gambar yang diperbolehkan',
-            'thumbnail.mimes'=>'Ekstensi file yang diperbolehkan: jpeg, png, jpg',
-            'thumbnail.max'=>'Ukuran file maksimal 10MB',
+            'title.required' => 'Judul wajib diisi',
+            'content.required' => 'Konten wajib diisi',
+            'thumbnail.image' => 'Hanya file gambar yang diperbolehkan',
+            'thumbnail.mimes' => 'Ekstensi file yang diperbolehkan: jpeg, png, jpg',
+            'thumbnail.max' => 'Ukuran file maksimal 10MB',
         ]);
 
         // Cek apakah ada file thumbnail yang dikirim
-        if($request->hasFile('thumbnail')){
+        if ($request->hasFile('thumbnail')) {
 
             // Hapus file thumbnail yang lama
-            if(isset($post->thumbnail) && file_exists(public_path(getenv('CUSTOM_THUMBNAIL_LOCATION')).$post->thumbnail)){
-                unlink(public_path(getenv('CUSTOM_THUMBNAIL_LOCATION')).$post->thumbnail);
+            if (isset($post->thumbnail) && file_exists(public_path(getenv('CUSTOM_THUMBNAIL_LOCATION')) . $post->thumbnail)) {
+                unlink(public_path(getenv('CUSTOM_THUMBNAIL_LOCATION')) . $post->thumbnail);
             }
 
             $image = $request->file('thumbnail');
-            $imageName = time().'_'.$image->getClientOriginalName();
+            $imageName = time() . '_' . $image->getClientOriginalName();
             $destinationPath = public_path(getenv('CUSTOM_THUMBNAIL_LOCATION'));
             $image->move($destinationPath, $imageName);
         }
 
         // Update data
-        $data=[
-            'title'=>$request->title,
-            'description'=>$request->description,
-            'content'=>$request->content,
-            'status'=>$request->status,
-            'thumbnail'=>isset($imageName) ? $imageName : $post->thumbnail,
-            'slug'=>$this->generateSlug($request->title, $post->id)
+        $data = [
+            'title' => $request->title,
+            'description' => $request->description,
+            'content' => $request->content,
+            'status' => $request->status,
+            'thumbnail' => isset($imageName) ? $imageName : $post->thumbnail,
+            'slug' => $this->generateSlug($request->title, $post->id)
         ];
 
         // Update data ke database
@@ -163,15 +164,15 @@ class BlogController extends Controller
         Gate::authorize('delete', $post);
 
         // Hapus file thumbnail
-        $thumbnailPath = public_path(getenv('CUSTOM_THUMBNAIL_LOCATION')).$post->thumbnail;
+        $thumbnailPath = public_path(getenv('CUSTOM_THUMBNAIL_LOCATION')) . $post->thumbnail;
 
         if (isset($post->thumbnail) && file_exists($thumbnailPath)) {
 
             // Tambahkan log untuk memeriksa path
-            \Log::info('Deleting thumbnail at path: ' . $thumbnailPath);
+            Log::info('Deleting thumbnail at path: ' . $thumbnailPath);
             unlink($thumbnailPath);
         }
-        
+
         Post::where('id', $post->id)->delete();
 
         return redirect()->route('member.blogs.index')->with('success', 'Data berhasil dihapus');
@@ -179,13 +180,14 @@ class BlogController extends Controller
     }
 
 
-    private function generateSlug( $title, $id=null){
+    private function generateSlug($title, $id = null)
+    {
         $slug = Str::slug($title);
-        $count = Post::where('slug', $slug)->when($id, function($query, $id){
+        $count = Post::where('slug', $slug)->when($id, function ($query, $id) {
             return $query->where('id', '!=', $id);
         })->count();
 
-        if($count > 0){
+        if ($count > 0) {
             $slug = $slug . '-' . ($count + 1);
         }
 
