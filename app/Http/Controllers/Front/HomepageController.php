589c0192c4dd53b6991d268ec3258a3f57cf8c2d
Benony Gabriel <gbenn.work@gmail.com>
2024-12-05 14:45:09 +0700

    feat(controller): implement index method in HomepageController to fetch published posts

--- app/Http/Controllers/Front/HomepageController.php
+++ app/Http/Controllers/Front/HomepageController.php
@@ -2,10 +2,16 @@
 
 namespace App\Http\Controllers\Front;
 
-use App\Http\Controllers\Controller;
+use App\Models\Post;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class HomepageController extends Controller
 {
-    //
+    public function index()
+    {
+        $data = Post::where('status', 'publish')->orderBy('id', 'desc')->paginate(5);
+
+        return view('components/front.home-page', compact('data'));
+    }
 }
