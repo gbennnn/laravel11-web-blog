1058fd5bd8bdaa8b5d015d6ac8f1bfc27fa8c5aa
Benony Gabriel <gbenn.work@gmail.com>
2024-12-06 19:39:35 +0700

    feat(blog): enhance blog detail view with dynamic content and metadata

--- resources/views/components/front/blog-detail.blade.php
+++ resources/views/components/front/blog-detail.blade.php
@@ -1,26 +1,33 @@
 <x-front.layout>
+    <x-slot name="title">{{ $data->title }}</x-slot>
+    <x-slot name="description">{{ $data->description }}</x-slot>
+    <x-slot name="pageBackground">{{ asset(getenv('CUSTOM_THUMBNAIL_LOCATION') . $data->thumbnail) }}</x-slot>
+    <x-slot name="pageHeaderLink">{{ route('blog-detail', ['slug' => $data->slug]) }}</x-slot>
+
     <!-- Main Content-->
     <div class="container px-4 px-lg-5">
         <div class="row gx-4 gx-lg-5 justify-content-center">
             <div class="col-md-10 col-lg-8 col-xl-7">
-                @foreach ($data as $key=>$value)
-                    <!-- Post preview-->
-                    <x-front.blog-list title='{{ $value->title }}' description='{{ $value->description }}' date="{{ $value->created_at->isoFormat('dddd, D MMMM Y') }}" user='{{ $value->user->name }}' link="{{ route('blog-detail', ['slug'=>$value->slug]) }}" />
-                @endforeach
+
+                {!! $data->content !!}
+
+                {{-- {{ $data->user->name }}'
+                link="{{ route('blog-detail', ['slug' => $data->slug]) }}" /> --}}
+
 
                 <!-- Pager-->
                 <div class="d-flex justify-content-between mb-4">
                     <div>
-                        @if(!$data->onFirstPage())
-                            <a class="btn btn-primary text-uppercase"
-                            href="{{ $data->previousPageUrl() }}">&larr;Newer Posts</a>
-                        @endif
+                        {{-- @if (!$data->onFirstPage())
+                            <a class="btn btn-primary text-uppercase" href="{{ $data->previousPageUrl() }}">&larr;Newer
+                                Posts</a>
+                        @endif --}}
                     </div>
                     <div>
-                        @if($data->hasMorePages())
-                            <a class="btn btn-primary text-uppercase"
-                            href="{{ $data->nextPageUrl() }}">Older Posts &rarr;</a>
-                        @endif
+                        {{-- @if ($data->hasMorePages())
+                            <a class="btn btn-primary text-uppercase" href="{{ $data->nextPageUrl() }}">Older Posts
+                                &rarr;</a>
+                        @endif --}}
                     </div>
 
                 </div>
